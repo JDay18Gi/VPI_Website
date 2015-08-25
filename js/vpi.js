adb1827b21ad76c1dc9456ce29e71755bf308572
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-08-24 23:06:35 -0400

    continue work on responsive menu

--- js/vpi.js
+++ js/vpi.js
@@ -2,23 +2,23 @@
 $(document).ready(function(){
 
 	/* drop down for "solutions"*/
-	$("#menu-solution").on("mouseenter", function(){
+	$("#solution-ul").on("mouseenter", function(){
 		$("#solution-dd").show();
 	});
-	$("#menu-solution").on("mouseleave", function(){
+	$("#solution-ul").on("mouseleave", function(){
 		$("#solution-dd").hide();
 	});
-
+/*
 	$("#solution-dd").on("mouseenter", function(){
 		$("#solution-dd").show();
 	});
 	$("#solution-dd").on("mouseleave", function(){
 		$("#solution-dd").hide();
 	});
-
+*/
 	/* drop down for mobile menu*/
-	$("#mobile-menu img").on("click", function(){
-		$("#mobile-dd").toggle();
+	$(".hamburger").on("click", function(){
+		$("nav").toggle();
 	});
 	/*
 	$("#menu-solution").on("mouseleave", function(){
