aa1282733e1ee1f95a0f5427b1cea481ef82c3d8
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-09-11 23:43:51 -0400

    Navigation Work

--- js/vpi.js
+++ js/vpi.js
@@ -1,59 +1,21 @@
 $(document).ready(function () {
 
-    /*Full-Size Load Width */
-    //On Load
-
-    if ($(this).width() >= 726) {
-
+    /*Full-Size Load*/
         $("#solution-ul").on("mouseenter", function () {
-            $("#solution-dd").show();
+          if ($(window).width() > 720) {
+              $("#solution-dd").show();
+          }
         });
+
         $("#solution-ul").on("mouseleave", function () {
+          if ($(window).width() > 720) {
             $("#solution-dd").hide();
+          }
         });
-    }
 
-    //On Resize
-    $(window).resize(function () {
-        if ($(this).width() >= 726) {
-
-            $("#solution-ul").on("mouseenter", function () {
-                $("#solution-dd").show();
-            });
-            $("#solution-ul").on("mouseleave", function () {
-                $("#solution-dd").hide();
-            });
-        }
+    /* Hamburger drop down for mobile menu*/
+    $(".hamburger").on("click", function () {
+      $("nav").toggle();
+      $("#solution-dd").show();
     });
-
-
-    /* Show Soluiton SubmMenu For Mobile*/
-    //On Load
-    if ($(this).width() < 726) {
-        $("#solution-dd").show();
-    }
-
-
-  //On Resize
-  $(window).resize(function () {
-      if ($(this).width() < 726) {
-          $("#solution-dd").show();
-      }
   });
-
-  $(window).resize(function () {
-      if ($(this).width() >= 726) {
-          $("#solution-dd").hide();
-      }
-  });
-
-
-
-
-/* Hamburger drop down for mobile menu*/
-$(".hamburger").on("click", function () {
-    $("nav").toggle();
-    $("#solution-dd").show();
-});
-
-});
